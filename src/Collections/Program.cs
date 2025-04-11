324f71a06c0d570bb4dd5258e6fd526ad49855a3
Cesar L Diaz <cesard@pbwweb.com>
2025-04-10 20:39:30 -0400

    Initialize project structure with .gitignore, solution file, and initial C# files

---
+++ src/Collections/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Collections;
+
+/// <summary>
+/// C# Language Features - Collections
+/// </summary>
+internal class Program
+{
+    protected Program()
+    {
+    }
+
+    public static void Main()
+    {
+        var people = Collections.People;
+
+        // given the following example of a Person to Be Found by name, update Age to 60 and print the updated record (write example below, explain the result of the code)
+        var personToBeFound = people.Single(p => p.Name.Equals("Moana"));
+
+        // given an index 27, remove the person from the list (explain why this is not a good idea, and what's the result of the following code)
+        var personToRemove = people.ElementAt(27);
+
+        Console.WriteLine(personToRemove);
+    }
+}
+
+#region Helpers
+
+public record Person(string Name, int Age, string Department, string City);
+
+public static class Collections
+{
+    public static IReadOnlyCollection<Person> People =>
+    [
+        new Person("John", 25, "HR", "New York"),
+        new Person("Jane", 30, "IT", "Los Angeles"),
+        new Person("Alice", 35, "Finance", "Chicago"),
+        new Person("Bob", 40, "HR", "Houston"),
+        new Person("Charlie", 45, "IT", "Phoenix"),
+        new Person("David", 50, "Finance", "Philadelphia"),
+        new Person("Eve", 55, "HR", "San Antonio"),
+        new Person("Frank", 35, "IT", "San Diego"),
+        new Person("Grace", 45, "Finance", "Dallas"),
+        new Person("Henry", 47, "HR", "San Jose"),
+        new Person("Ivy", 35, "IT", "Austin"),
+        new Person("Jack", 56, "Finance", "Jacksonville"),
+        new Person("Kelly", 45, "HR", "Indianapolis"),
+        new Person("Larry", 38, "IT", "San Francisco"),
+        new Person("Mary", 46, "Finance", "Columbus"),
+        new Person("Nancy", 55, "HR", "Fort Worth"),
+        new Person("Oliver", 44, "IT", "Charlotte"),
+        new Person("Peter", 55, "Finance", "Detroit"),
+        new Person("Quinn", 52, "HR", "El Paso"),
+        new Person("Rose", 29, "IT", "Seattle"),
+        new Person("Sam", 42, "Finance", "Denver"),
+        new Person("Tom", 33, "HR", "Washington"),
+        new Person("Ursula", 35, "IT", "Boston"),
+        new Person("Victor", 40, "Finance", "Memphis"),
+        new Person("Wendy", 45, "HR", "Nashville")
+    ];
+}
+
+#endregion Helpers
---
+++ src/Ef.Core/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ef.Core;
+
+/// <summary>
+/// EF Core Helpers
+/// </summary>
+internal class Program
+{
+    protected Program()
+    {
+    }
+
+    private static void Main()
+    {
+        /*
+                       1. Provide migrations support to current project
+
+                       2. Add all people to the database
+
+                       3. Read all people from the database
+
+                       4. Update a record by finding by name in the list and updating the Age, print the updated record
+
+                       5. Group by Department
+
+                       6. Traverse the resulting groups and print the department name and the number of people in that department
+        */
+
+        Console.WriteLine("Done!");
+    }
+
+    #region Helpers
+
+    private interface IEntity
+    {
+        Guid Id { get; }
+    }
+
+    public class Person : IEntity
+    {
+        protected Person()
+        {
+        }
+
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Department { get; private set; }
+        public string City { get; private set; }
+
+        public static Person Create(string name, int age, string department, string city) => new(name, age, department, city);
+
+        private Person(string name, int age, string department, string city)
+        {
+            Name = name;
+            Age = age;
+            Department = department;
+            City = city;
+        }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        // 7. Define a DbSet for the Person entity
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // 8. Improve performance of the app by implementing split queries
+
+            optionsBuilder.UseSqlite("Data Source=app.db");
+        }
+    }
+
+    public static class Collections
+    {
+        public static IReadOnlyCollection<Person> People =>
+        [
+            Person.Create("John", 25, "HR", "New York"),
+            Person.Create("Jane", 30, "IT", "Los Angeles"),
+            Person.Create("Alice", 35, "Finance", "Chicago"),
+            Person.Create("Bob", 40, "HR", "Houston"),
+            Person.Create("Charlie", 45, "IT", "Phoenix"),
+            Person.Create("David", 50, "Finance", "Philadelphia"),
+            Person.Create("Eve", 55, "HR", "San Antonio"),
+            Person.Create("Frank", 35, "IT", "San Diego"),
+            Person.Create("Grace", 45, "Finance", "Dallas"),
+            Person.Create("Henry", 47, "HR", "San Jose"),
+            Person.Create("Ivy", 35, "IT", "Austin"),
+            Person.Create("Jack", 56, "Finance", "Jacksonville"),
+            Person.Create("Kelly", 45, "HR", "Indianapolis"),
+            Person.Create("Larry", 38, "IT", "San Francisco"),
+            Person.Create("Mary", 46, "Finance", "Columbus"),
+            Person.Create("Nancy", 55, "HR", "Fort Worth"),
+            Person.Create("Oliver", 44, "IT", "Charlotte"),
+            Person.Create("Peter", 55, "Finance", "Detroit"),
+            Person.Create("Quinn", 52, "HR", "El Paso"),
+            Person.Create("Rose", 29, "IT", "Seattle"),
+            Person.Create("Sam", 42, "Finance", "Denver"),
+            Person.Create("Tom", 33, "HR", "Washington"),
+            Person.Create("Ursula", 35, "IT", "Boston"),
+            Person.Create("Victor", 40, "Finance", "Memphis"),
+            Person.Create("Wendy", 45, "HR", "Nashville")
+        ];
+    }
+
+    #endregion Helpers
+}
